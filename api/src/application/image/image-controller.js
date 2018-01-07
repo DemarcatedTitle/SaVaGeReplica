9893092f286fa9d362c04da9b718d9caa2a8817b
dt <dimmton@gmail.com>
2018-01-07 12:01:23 -0700

    Add src dir and image routes/handlers

---
+++ api/src/application/image/image-controller.js
@@ -0,0 +1,122 @@
+const fs = require('fs');
+const child_process = require('child_process');
+const uuidv4 = require('uuid/v4');
+// const knexfile = require('../../../knexfile.js');
+// const knex = require('../../lib/knex.js');
+var redis = require('redis'),
+  client = redis.createClient(6379, 'redis');
+const uploadID = uuidv4();
+module.exports = {
+  get: (request, h, err) => {
+    return new Promise(function(resolve) {
+      if (err) {
+        throw err;
+      }
+      // if you'd like to select database 3, instead of 0 (default), call
+      // client.select(3, function() { /* ... */ });
+      //
+      // If client.get uuid is integer, return integer for progress indicator
+      //
+      // If client.get uuid is 'finished'
+      // get image from db based on uuid
+      //
+
+      client.on('error', function(err) {
+        console.log('Error ' + err);
+      });
+      client.get(uploadID, function(err, results) {
+        if (results !== null) {
+          const percent = parseInt(results * 100);
+          console.log(percent);
+          resolve(h.response({ progress: percent.toString() }));
+        } else {
+          resolve(h.response('error'));
+        }
+      });
+    });
+  },
+  uploadImage: function(request, h, err) {
+    return new Promise(function(resolve) {
+      if (err) {
+        throw err;
+      }
+      const imageID = uuidv4();
+      console.log(`imageID is: \n\n${imageID}\n\n`);
+      let defaults = {};
+      defaults.numofshapes = 10;
+      defaults.rep = 0;
+      defaults.nth = 0;
+      defaults.mode = 1;
+      fs.writeFile('./images' + imageID, request.payload.file, err => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+        function numShapes(numofshapes) {
+          if (
+            typeof request.payload.numofshapes === 'number' &&
+            request.payload.numofshapes < 10000
+          ) {
+            return request.payload.numofshapes;
+          } else {
+            return defaults.numofshapes;
+          }
+        }
+        function mode(mode) {
+          if (
+            typeof request.payload.mode === 'integer' &&
+            request.payload.mode < 9
+          ) {
+            return request.payload.mode;
+          } else {
+            return defaults.mode;
+          }
+        }
+        const shapes = numShapes(request.payload.numofshapes);
+        // console.log(typeof shapes);
+        const command = `primitive -i ${imageID} -n ${shapes} -m 3 -v -o ./images/${imageID}.svg`;
+        console.log(command);
+        //
+        // Generate a uuid, reply with that uuid
+        // Run the command with that uuid as a filename
+        //
+        child_process
+          .exec(command, function(error, stdout, stderr) {
+            //
+            // Process is complete, insert into DB based on uuid
+            // Then update redis to indicate this
+            //
+            if (error) {
+              console.error(`exec error: ${error}`);
+            }
+            var yoursvg;
+            yoursvg = stdout.substring(
+              stdout.indexOf('<svg'),
+              stdout.indexOf('</svg>') + 6
+            );
+            console.log(`stderr: ${stderr}`);
+            // const response = h.response(yoursvg);
+            // response.type('image/svg+xml');
+            // return response;
+          })
+          .stdout.on('data', function(data) {
+            //
+            // While process is running update progress on redis
+            //
+            const currentStep = parseInt(data.slice(0, data.indexOf(':')));
+            // console.log(currentStep);
+            // console.log(``);
+            console.log(`currentStep: ${currentStep}\nshapes: ${shapes}`);
+            const progress = isNaN(currentStep) ? 0 : currentStep / shapes;
+            console.log(progress);
+            client.set(uploadID, progress);
+            client.set(uploadID, progress);
+          });
+        client.set(uploadID, 0, function() {
+          resolve(h.response({ uploadID: uploadID }));
+        });
+      });
+    });
+  },
+};
+
+module.exports['@singleton'] = true;
+module.exports['@require'] = ['server'];
---
+++ api/src/application/image/image-routes.js
@@ -0,0 +1,82 @@
+// const Joi = require('joi');
+const imagecontroller = require('./image-controller.js');
+module.exports = [
+  {
+    method: 'GET',
+    path: '/image/{uploadID}',
+    handler: imagecontroller.get,
+    // config: {
+    //   tags: ['api'],
+    //   description: 'An example endpoint using a controller',
+    // },
+  },
+  {
+    method: 'GET',
+    path: '/image',
+    handler: (request, h, err) => {
+      if (err) throw err;
+      return h.response('Hey');
+      // return h.response('GET /image');
+    },
+    // config: {
+    //   tags: ['api'],
+    //   description: 'An example endpoint using a controller',
+    // },
+  },
+  {
+    method: 'POST',
+    path: '/api/image',
+    config: {
+      tags: ['api'],
+      description: 'Image upload route',
+      // validate: {
+      //   payload: {
+      //     outputfilename: Joi.string(),
+      //     numofshapes: Joi.number().integer(),
+      //     mode: Joi.number().integer(),
+      //     rep: Joi.number().integer(),
+      //     nth: Joi.number().integer(),
+      //     outputsize: Joi.number().integer(),
+      //     alpha: Joi.number().integer(),
+      //     backgroundcolor: Joi.number().integer(),
+      //     file: Joi.binary(),
+      //     filename: Joi.string(),
+      //   },
+      // },
+    },
+    handler: imagecontroller.uploadImage,
+  },
+  // {
+  //   method: 'GET',
+  //   path: '/async-validation-example',
+  //   handler: (request, reply) => {
+  //     reply({ message: 'the request was valid!' });
+  //   },
+  //   config: {
+  //     tags: ['api'],
+  //     description: 'An example endpoint using async validation',
+  //     validate: {
+  //       query: asyncValidation(
+  //         {
+  //           key: Joi.string().required(),
+  //         },
+  //         {
+  //           key: (value, context) => Promise.resolve(value),
+  //         }
+  //       ),
+  //     },
+  //   },
+  // },
+  // {
+  //   method: 'GET',
+  //   path: '/example-auth',
+  //   handler: (request, reply) => {
+  //     reply({ message: 'an authenticated reply!' });
+  //   },
+  //   config: {
+  //     auth: 'jwt',
+  //     tags: ['api'],
+  //     description: 'An example of an authenticated enpoint',
+  //   },
+  // },
+];
