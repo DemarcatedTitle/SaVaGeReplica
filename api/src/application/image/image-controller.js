035026f4e2fe5212dd88f4d6be70979971bb756f
dt <dimmton@gmail.com>
2018-01-18 21:20:24 -0700

    Change server to use fs instead of db for now

--- api/src/application/image/image-controller.js
+++ api/src/application/image/image-controller.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const child_process = require('child_process');
 const uuidv4 = require('uuid/v4');
 const imageDB = require('../../../imageDB.js');
+const path = require('path');
 // const knexfile = require('../../../knexfile.js');
 // const knex = require('../../lib/knex.js');
 var redis = require('redis'),
@@ -37,6 +38,8 @@ module.exports = {
               if (thesvg == []) {
                 resolve(h.response({ progress: percent.toString() }));
               } else {
+                let path = `src/application/image/images/${uploadID}.svg`;
+                resolve(h.file(path));
                 const response = h.response(thesvg[0].image);
                 response.type('image/svg+xml');
                 resolve(h.response(response));
@@ -54,17 +57,19 @@ module.exports = {
   },
   getUploaded: (request, h, err) => {
     const uploadID = request.params.uploadID;
-    return new Promise(function(resolve) {
-      if (err) {
-        throw err;
-      }
-      const yoursvg = imageDB.getImage(uploadID);
-      yoursvg.then(thesvg => {
-        const response = h.response(thesvg[0].image);
-        response.type('image/svg+xml');
-        resolve(h.response(response));
-      });
-    });
+    let path = `src/application/image/images/${uploadID}.svg`;
+    return h.file(path);
+    // return new Promise(function(resolve) {
+    //   if (err) {
+    //     throw err;
+    //   }
+    //   const yoursvg = imageDB.getImage(uploadID);
+    //   yoursvg.then(thesvg => {
+    //     const response = h.response(thesvg[0].image);
+    //     response.type('image/svg+xml');
+    //     resolve(h.response(response));
+    //   });
+    // });
   },
   getAll: (request, h, err) => {
     return new Promise(function(resolve) {
@@ -89,94 +94,110 @@ module.exports = {
       defaults.nth = 0;
       defaults.mode = 1;
       defaults.name = 'Your Picture';
-      fs.writeFile('./images/' + imageID, request.payload.file, err => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-        function numShapes(numofshapes) {
-          if (
-            typeof parseInt(numofshapes) === 'number' &&
-            numofshapes < 10000
-          ) {
-            return numofshapes;
-          } else {
-            return defaults.numofshapes;
-          }
-        }
-        function mode(mode) {
-          console.log(mode);
-          if (
-            typeof parseInt(request.payload.mode) === 'number' &&
-            request.payload.mode < 9
-          ) {
-            return request.payload.mode;
-          } else {
-            return defaults.mode;
-          }
-        }
-        function outputName(name) {
-          if (request.payload.outputfilename) {
-            return request.payload.outputfilename;
-          } else {
-            return defaults.name;
-          }
-        }
-        const shapes = numShapes(request.payload.numofshapes);
-        // console.log(typeof shapes);
-        const command = `primitive -i ./images/${imageID} -n ${shapes} -m ${mode(
-          request.payload.mode
-        )} -v -o ./images/${imageID}.svg`;
-        console.log(command);
-        //
-        // Generate a uuid, reply with that uuid
-        // Run the command with that uuid as a filename
-        //
-        child_process
-          .exec(command, function(error, stdout, stderr) {
-            //
-            // Process is complete, insert into DB based on uuid
-            // Then update redis to indicate this
-            //
-            if (error) {
-              console.error(`exec error: ${error}`);
+      fs.writeFile(
+        './src/application/image/images/' + imageID,
+        request.payload.file,
+        err => {
+          if (err) throw err;
+          console.log('The file has been saved!');
+          function numShapes(numofshapes) {
+            if (
+              typeof parseInt(numofshapes) === 'number' &&
+              numofshapes < 10000
+            ) {
+              return numofshapes;
+            } else {
+              return defaults.numofshapes;
             }
-            var yoursvg;
-            yoursvg = stdout.substring(
-              stdout.indexOf('<svg'),
-              stdout.indexOf('</svg>') + 6
-            );
-            console.log(`stderr: ${stderr}`);
-            // const response = h.response(yoursvg);
-            // response.type('image/svg+xml');
-            // return response;
-            const dbImageShape = {
-              image: yoursvg,
-              name: outputName(request.payload.outputfilename),
-              image_id: imageID,
-            };
-            imageDB.addImage(dbImageShape);
-          })
-          .stdout.on('data', function(data) {
-            //
-            // While process is running update progress on redis
-            //
-            const currentStep = parseInt(data.slice(0, data.indexOf(':')));
-            // console.log(currentStep);
-            // console.log(``);
-            //
-            //
-            // TODO change it so it doesn't undo setting it to 1
-            //
-            console.log(`currentStep: ${currentStep}\nshapes: ${shapes}`);
-            const progress = isNaN(currentStep) ? 0 : currentStep / shapes;
-            console.log(progress);
-            if (!isNaN(currentStep)) {
-              client.set(imageID, progress);
+          }
+          function rep(rep) {
+            if (typeof parseInt(rep) === 'number' && rep < 10000) {
+              return rep;
+            } else {
+              return defaults.rep;
             }
+          }
+          function mode(mode) {
+            console.log(mode);
+            if (
+              typeof parseInt(request.payload.mode) === 'number' &&
+              request.payload.mode < 9
+            ) {
+              return request.payload.mode;
+            } else {
+              return defaults.mode;
+            }
+          }
+          function outputName(name) {
+            if (request.payload.outputfilename) {
+              return request.payload.outputfilename;
+            } else {
+              return defaults.name;
+            }
+          }
+          const shapes = numShapes(request.payload.numofshapes);
+          // console.log(typeof shapes);
+          const command = `foglemanPrimitive -i ./src/application/image/images/${imageID} -n ${shapes} -rep ${rep(
+            request.payload.rep
+          )} -m ${mode(
+            request.payload.mode
+          )} -v -bg ff5733 -o ./src/application/image/images/${imageID}.svg`;
+          console.log(command);
+          //
+          // Generate a uuid, reply with that uuid
+          // Run the command with that uuid as a filename
+          //
+          child_process
+            .exec(command, function(error, stdout, stderr) {
+              //
+              // Process is complete, insert into DB based on uuid
+              // Then update redis to indicate this
+              //
+              if (error) {
+                console.error(`exec error: ${error}`);
+              }
+              var yoursvg;
+              yoursvg = stdout.substring(
+                stdout.indexOf('<svg'),
+                stdout.indexOf('</svg>') + 6
+              );
+              console.log(`stderr: ${stderr}`);
+              // const response = h.response(yoursvg);
+              // response.type('image/svg+xml');
+              // return response;
+              const dbImageShape = {
+                image: yoursvg,
+                name: outputName(request.payload.outputfilename),
+                image_id: imageID,
+              };
+              imageDB.addImage(dbImageShape);
+            })
+            .stdout.on('data', function(data) {
+              //
+              // While process is running update progress on redis
+              //
+              const currentStep = parseInt(data.slice(0, data.indexOf(':')));
+              // console.log(currentStep);
+              // console.log(``);
+              //
+              //
+              // TODO change it so it doesn't undo setting it to 1
+              //
+              console.log(`currentStep: ${currentStep}\nshapes: ${shapes}`);
+              const progress = isNaN(currentStep) ? 0 : currentStep / shapes;
+              console.log(progress);
+              if (!isNaN(currentStep)) {
+                if (progress > 100) {
+                  console.log('\n\nprogress is over 100 \n\n');
+                }
+                client.set(imageID, progress);
+              }
+            });
+          client.set(imageID, 0, function() {
+            resolve(h.response({ uploadID: imageID }));
           });
-        client.set(imageID, 0, function() {
-          resolve(h.response({ uploadID: imageID }));
-        });
-      });
+        }
+      );
     });
   },
 };
