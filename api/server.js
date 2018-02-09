7c09315a466b2971488010bb5866afd255a175c6
dt <dimmton@gmail.com>
2018-02-09 06:04:41 -0700

    Change server to use async server

--- api/server.js
+++ api/server.js
@@ -1,12 +1,18 @@
 const Hapi = require('hapi');
 const imageRoutes = require('./src/application/image/image-routes.js');
 const imageDB = require('./imageDB.js');
+const Inert = require('inert');
+
 imageDB.start();
-const server = new Hapi.Server({ port: 9000 });
 
-server.route(imageRoutes);
+async function SavageServer() {
+  const server = new Hapi.Server({ port: 9000 });
+  await server.register(Inert);
+  server.route(imageRoutes);
+  await server.start();
+  console.log('Server started at: ' + server.info.uri);
+  // .then(test => console.log('Server started at: ' + server.info.uri))
+  // .catch(err => console.log(err));
+}
 
-server
-  .start()
-  .then(test => console.log('Server started at: ' + server.info.uri))
-  .catch(err => console.log(err));
+SavageServer();
